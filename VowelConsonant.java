1c3ada015941c162050f41e0b742187b8e8c89e8
Arthi1998 <43901606+Arthi1998@users.noreply.github.com>
2019-02-12 10:28:29 +0530

    Update VowelConsonant.java

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -6,7 +6,7 @@ public class VowelConsonant {
 
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " is vowel");
-        if else(ch=='$')
+        else if(ch=='$')
             System.out.println(ch + " is invalid");
         else
             System.out.println(ch+ "is consonant");
